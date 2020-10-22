e2dd2b44ef769c54e9af9eea1154c32ab2741d0c
cwsvensson <cws.commercial@gmail.com>
2020-10-22 13:02:17 -0400

    Day 3 task - last letter capital

---
+++ My Programs/string_demos/src/examples/TitleCaseDemo.java
@@ -0,0 +1,27 @@
+package examples;
+
+public class TitleCaseDemo {
+
+	public static void main(String[] args) {
+		String s="hello hi good morning I am enjoying programming with java";
+		StringBuilder sb1=new StringBuilder();
+		String ar[]=s.split(" ");
+		for(String s1:ar) {
+			sb1.append(Character.toUpperCase(s1.charAt(0))).append(s1.substring(1)).append(" ");
+		}
+		System.out.println(sb1.toString().trim());
+		
+		StringBuilder sb2=new StringBuilder();
+		
+		for(String s1:ar) {
+			if (s1.length()>=2) {
+			 sb2.append(s1.substring(0,s1.length()-1)).append(Character.toUpperCase(s1.charAt(s1.length()-1))).append(" ");
+			} else
+				sb2.append(Character.toUpperCase(s1.charAt(0))).append(" ");
+		    }
+		System.out.println(sb2.toString().trim());
+		
+	}
+
+}
+//Task - Convert every word's last char to uppercase
